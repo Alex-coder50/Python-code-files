c6c90f44de3e67309221deb5091da1bb5f0abb3a
Alex-coder50 <thehonoredone995@gmail.com>
2026-02-14 21:01:28 +0200

    Add files via upload

---
+++ l3/a3.py
@@ -0,0 +1,11 @@
+a=4
+print('The data type of a is:', type(a))
+
+s=6.3
+print('The data type of s is:', type(s))
+
+d="code"
+print('The data type of d is:', type(d))
+
+e=True
+print('The data type of e is:', type(e))
---
+++ l3/a3_1.py
@@ -0,0 +1,11 @@
+f  = 34
+print("The data type of f before type casting is:", type(f))
+
+j = float(f)
+print("The data type of f after type casting is:", type(j))
+
+v = "83"
+print("The data type of v before type casting is:", type(v))
+
+z = float(v)
+print("The data type of v after type casting is:", type(z))
