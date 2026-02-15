9881eb8421d253586c401798a37af95ae236fc13
Alex-coder50 <thehonoredone995@gmail.com>
2026-02-15 20:39:26 +0200

    Add files via upload

---
+++ l4/a4_3.py
@@ -0,0 +1,11 @@
+"""
+Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?
+"""
+maths = 40
+science = 70
+hindi = 70
+english = 60
+
+total = maths + science + hindi + english
+percentage = (total / 400) * 100
+print("The percentage is:",percentage)
