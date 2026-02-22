3c9eb84f50675f07a23d7af7894b65c1624d744e
Alex-coder50 <thehonoredone995@gmail.com>
2026-02-22 20:54:01 +0200

    Add files via upload

---
+++ l6/l6.py
@@ -0,0 +1,16 @@
+a = 36
+b = -12
+c = 100
+
+if a and b and c :
+    print("All of them have the boolean value as true ")
+else:
+    print("Atleast one of the value has a negative")
+
+a = -59
+b = 59
+c = 1
+if a>0 or b>0 or c>0:
+    print("Either of the condition is true")
+else:
+    print("All of them are false")
