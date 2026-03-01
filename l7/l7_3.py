0d0f2f6f57d240d0a130bdbdaab897069baeee91
Alex-coder50 <thehonoredone995@gmail.com>
2026-03-01 19:55:11 +0200

    Add files via upload

---
+++ l7/l7_3.py
@@ -0,0 +1,25 @@
+print("Choose your ride")
+print("1. Bike")
+print("2. Car")
+
+choice = int(input("Enter your choice: "))
+if choice == 1:
+    print("What type of bike?")
+    print("1. Scooty")
+    print("2. Scooter")
+    choice2 = int(input("Enter your choice: "))
+    if choice2 == 1:
+        print("You selected a scooty")
+    else:
+        print("You selected a scooter")
+elif choice == 2:
+    print("What type of car?")
+    print("1. Honda")
+    print("2. SUV")
+    choice3 = int(input("Enter your choice: "))
+    if choice3 == 1:
+        print("You selected a Honda")
+    else:
+        print("You selected an SUV")
+else:
+    print("Invalid choice")
