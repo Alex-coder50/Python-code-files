2025756a72d773966b4c0af2e850382a8cfa8929
Alex-coder50 <thehonoredone995@gmail.com>
2026-03-15 19:21:36 +0200

    Delete l10/l10,1.py

--- l10/l10,1.py
+++
@@ -1,9 +0,0 @@
-string = input("enter a word: ")
-char = input("Enter a single character!")
-t = 0
-count = 0
-while t < len(string):
-    if string[t] == char:
-        count = count + 1
-    t = t + 1
-print("The character ",char, " has occured ",count," times in the string ",string )
